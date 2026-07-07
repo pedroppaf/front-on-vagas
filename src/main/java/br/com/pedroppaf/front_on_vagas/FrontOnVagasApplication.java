50ac2b8fb527e2556b44776dc1dfc77db1054da6
Pedro <pedroppaf@gmail.com>
2026-07-07 16:12:51 -0300

    beginning of the creation of the on-vagas frontend with thymeleaf

---
+++ src/main/java/br/com/pedroppaf/front_on_vagas/FrontOnVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedroppaf.front_on_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontOnVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontOnVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedroppaf/front_on_vagas/FrontOnVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedroppaf.front_on_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontOnVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
