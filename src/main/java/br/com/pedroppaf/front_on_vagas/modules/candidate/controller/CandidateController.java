1b37571b2ac08db1baab0fe838422810f813af36
Pedro <pedroppaf@gmail.com>
2026-07-09 21:45:22 -0300

    login and profile pages added

--- src/main/java/br/com/pedroppaf/front_on_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/pedroppaf/front_on_vagas/modules/candidate/controller/CandidateController.java
@@ -1,15 +1,39 @@
 package br.com.pedroppaf.front_on_vagas.modules.candidate.controller;
 
+import br.com.pedroppaf.front_on_vagas.modules.candidate.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateService candidateService;
+
     @GetMapping("/login")
     public String login() {
         return "candidate/login";
     }
+
+    @PostMapping("/singIn")
+    public String singIn(RedirectAttributes redirectAttributes, String username, String password){
+        try {
+            var token = this.candidateService.login(username, password);
+
+            return "candidate/profile";
+
+        }catch (HttpClientErrorException e) {
+            redirectAttributes.addAttribute("error_message", "Usuário/Senha incorretos");
+            return "redirect:/candidate/login";
+        }
+    }
+
+
+
 }
---
+++ src/main/java/br/com/pedroppaf/front_on_vagas/modules/candidate/dto/Token.java
@@ -0,0 +1,14 @@
+package br.com.pedroppaf.front_on_vagas.modules.candidate.dto;
+
+public class Token {
+
+    private String access_token;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+}
